d267b63811b002360807c82961569c367961ba07
Nicholas Sandomeno <nicholassandomeno@Nicholass-MBP.fios-router.home>
2022-12-15 04:13:25 -0500

    pleb wallet - LN tutorial

---
+++ src/components/Button.js
@@ -0,0 +1,35 @@
+import PaymentsModal from "./PaymentModal";
+import React, { useState } from "react";
+import "./Button.css";
+
+export const Button = () => {
+    const [modalState, setModalState] = useState({
+        type: "",
+        open: false,
+    });
+
+    return (
+        <div>
+            <div className="buttons">
+                <button
+                    className="button"
+                    onClick={() =>
+                        setModalState({ type: "send", open: true })
+                    }
+                >
+                    Send
+                </button>
+                <button
+                    className="button"
+                    onClick={() =>
+                        setModalState({ type: "receive", open: true })
+                    }
+                >
+                    Receive
+                </button>
+            </div>
+        <PaymentsModal modalState={modalState} setModalState={setModalState} />
+        </div>
+    );
+};
+export default Button;
